9543dce74f86e85db73152dceab2920c08aba70b
KAY S <KS.KWANRUAN@GMAIL.com>
2020-05-27 12:55:04 +1000

    add and delete recipe

--- app.js
+++ app.js
@@ -1,30 +1,69 @@
 const list = document.querySelector("ul");
 const form = document.querySelector("form");
 
-const addRecipe = (recipe) => {
+const addRecipe = (recipe, id) => {
   let time = recipe.created_at.toDate();
   let html = `
-    <li>
-        <div>${recipe.title}</div>
-        <div>${time}</div>
+    <li data-id="${id}">
+        <div>Recipe name:${recipe.title}</div>
+        <div>Created at:${time}</div>
+        <div>Author: ${recipe.author}</div>
+        <button class="btn btn-danger btn-sm my-2">delete</button>
     </li>`;
 
   list.innerHTML += html;
 };
 
-db.collection("recipes")
-  .get()
-  .then((snapshot) => {
-    snapshot.docs.forEach((doc) => {
-      addRecipe(doc.data());
-    });
-  })
-  .catch((err) => {
-    console.log(err);
+const deleteRecipe = (id) => {
+  const recipes = document.querySelectorAll("li");
+  recipes.forEach((recipe) => {
+    if (recipe.getAttribute("data-id") === id) {
+      recipe.remove();
+    }
   });
+};
+
+//get document real time
+db.collection("recipes").onSnapshot((snapshot) => {
+  snapshot.docChanges().forEach((change) => {
+    const doc = change.doc;
+    if (change.type === "added") {
+      addRecipe(doc.data(), doc.id);
+    } else if (change.type === "removed") {
+      deleteRecipe(doc.id);
+    }
+  });
+});
 
 //add documents
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   const now = new Date();
+  const author = "anonymous";
+  const recipe = {
+    title: form.recipe.value.trim(),
+    author: author,
+    created_at: firebase.firestore.Timestamp.fromDate(now),
+  };
+  db.collection("recipes")
+    .add(recipe)
+    .then(() => {
+      console.log("recipe added");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+// deleting data
+list.addEventListener("click", (e) => {
+  if (e.target.tagName === "BUTTON") {
+    const id = e.target.parentElement.getAttribute("data-id");
+    db.collection("recipes")
+      .doc(id)
+      .delete()
+      .then(() => {
+        console.log("recipe deleted");
+      });
+  }
 });
